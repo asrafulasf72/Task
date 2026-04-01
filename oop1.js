b4f1152dda11f6f18e56dcf23edf6798d8a8eb4a
Asraful Islam <asrafulasraful166@gmail.com>
2026-04-01 12:03:05 +0600

    Add files via upload

---
+++ oop1.js
@@ -0,0 +1,11 @@
+class player{
+    constructor(name, age, skill){
+        this.name=name;
+        this.age=age;
+        this.skill=skill;
+    }
+}
+
+const masrafi = new player("Masrafi", 26, "Bowler")
+
+console.log(masrafi.age)
