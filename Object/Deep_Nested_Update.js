24e740ee6720d4ffd6d927779d541f058dd12621
asraful <asrafulasraful166@gmail.com>
2026-06-05 22:07:52 +0600

    Deep Nested Update User Create

---
+++ Object/Deep_Nested_Update.js
@@ -0,0 +1,18 @@
+const users = [
+  {
+    id: 1,
+    name: "Rahim",
+    address: {
+      city: "Dhaka",
+      zip: 1200
+    }
+  },
+  {
+    id: 2,
+    name: "Karim",
+    address: {
+      city: "Khulna",
+      zip: 9000
+    }
+  }
+];
