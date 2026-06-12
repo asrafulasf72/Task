356150d5d63d1fd0235f646428218907e682b7dc
asraful <asrafulasraful166@gmail.com>
2026-06-12 23:14:03 +0600

    Nested Object Destructuring

--- Object/Nested_Object_Destructuring.js
+++ Object/Nested_Object_Destructuring.js
@@ -5,3 +5,12 @@ const user = {
     zip: 1200
   }
 };
+
+const {
+  name,
+  address: { city, zip }
+} = user;
+
+console.log(name); // Asraful
+console.log(city); // Dhaka
+console.log(zip);  // 1200
