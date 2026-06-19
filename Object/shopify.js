2bc790ba5ad8f8b73e9848cb8aee68f6c021c121
asraful <asrafulasraful166@gmail.com>
2026-06-19 23:45:40 +0600

    Social media schema added

--- Object/shopify.js
+++ Object/shopify.js
@@ -191,3 +191,556 @@ window.dataLayer.push({
   }
   });  
 });
+
+//view item event setup
+analytics.subscribe("product_viewed", (event) => {
+  console.log('custom event', event)
+
+var currencyToCountry = {
+  'USD': 'US', 'EUR': 'NL', 'BDT': 'BD', 'GBP': 'GB', 'CAD': 'CA', 'AUD': 'AU', 'JPY': 'JP',
+  'INR': 'IN', 'CNY': 'CN', 'SEK': 'SE', 'NOK': 'NO', 'CHF': 'CH', 'SGD': 'SG', 'BRL': 'BR',
+  'ZAR': 'ZA', 'RUB': 'RU', 'AED': 'AE', 'KRW': 'KR', 'MXN': 'MX', 'TRY': 'TR', 'MYR': 'MY',
+  'NZD': 'NZ', 'HKD': 'HK', 'DKK': 'DK', 'PLN': 'PL', 'THB': 'TH', 'EGP': 'EG', 'ILS': 'IL',
+  'IDR': 'ID', 'CZK': 'CZ', 'HUF': 'HU', 'ARS': 'AR', 'CLP': 'CL', 'PHP': 'PH', 'COP': 'CO',
+  'RSD': 'RS', 'QAR': 'QA', 'BHD': 'BH', 'KWD': 'KW', 'OMR': 'OM', 'PKR': 'PK', 'SAR': 'SA'
+};
+
+var products = event.data.productVariant;
+var itemsArray = [];
+var total = event.data.productVariant.price.amount;
+var currency = event.data.productVariant.price.currencyCode;
+var totalQuantity = 1;
+
+var item_variant = products.id;
+var item_id = products.product.id;
+var item_name = products.product.title;
+var item_category = products.product.type;
+var item_brand = products.product.vendor;
+var price = products.price.amount;
+var quantity = 1;
+
+var productObject = {
+  item_id: item_id,
+  item_variant: item_variant,
+  item_name: item_name,
+  item_category: item_category,
+  item_brand: item_brand,
+  item_delivery: 'in_store',
+  price: price,
+  quantity: quantity
+};
+
+itemsArray.push(productObject);
+
+
+// Get the country code based on the currency code
+var countryCode = currencyToCountry[currency] || 'US'; // Default to US if the currency is not found
+
+// Generate the dynamic Google Ads Remarketing id
+var gadsItems = itemsArray.map(function(product) {
+  return {
+    id: 'shopify_' + countryCode + '_' + product.item_id + '_' + product.item_variant,
+    google_business_vertical: 'retail'
+  };
+});
+
+// Create arrays and strings for content_ids, content_names, and content categories
+var contentId = itemsArray.map(function(product) {
+  return product.item_id;
+}).length === 1 ? itemsArray[0].item_id : itemsArray.map(product => product.item_id);
+
+var contentName = itemsArray.map(function(product) {
+  return product.item_name;
+}).length === 1 ? itemsArray[0].item_name : itemsArray.map(product => product.item_name);
+
+var uniqueCategories = [...new Set(itemsArray.map(product => product.item_category))];
+var contentCategory = uniqueCategories.length === 1 ? uniqueCategories[0] : uniqueCategories.join(', ');
+
+// Facebook schema data
+var fbItems = itemsArray.map(function(product) {
+  return {
+    id: product.item_id,
+    item_price: product.price,
+    delivery_category: product.item_delivery,
+    quantity: product.quantity || 1
+  };
+});
+
+// Tiktok contents build-up
+var tiktokItems = itemsArray.map(function(product) {
+  return {
+    content_id: product.item_id,
+    content_name: product.item_name,
+    content_category: product.item_category,
+    brand: product.item_brand,
+    price: product.price,
+    quantity: product.quantity || 1
+  };
+});
+
+// Pinterest contents array build-up
+var pinterestItems = itemsArray.map(function(product) {
+  return {
+    product_id: product.item_id,
+    product_name: product.item_name,
+    product_price: product.price,
+    product_brand: product.item_brand,
+    product_quantity: product.quantity || 1,
+    product_category: product.item_category,
+  };
+});
+
+// view cart dataLayer
+window.dataLayer.push({
+  event: 'view_item',
+  client_id: event.clientId,
+  event_id: event.id,
+  time_stamp: event.timestamp,
+  time_stamp_unix: Math.round(new Date(event.timestamp).getTime() / 1000), 
+  page_location: event.context.document.location.href,
+  page_path: event.context.document.location.pathname,
+  page_hostname: event.context.document.location.hostname,
+  fired_from: 'custom_event',
+  ecommerce: {
+    value: total,
+    currency: currency,
+    items: itemsArray
+  },
+  google_ads: {
+    remarketing: gadsItems
+  },
+  facebook: {
+    content_type: 'product_group',
+    content_ids: contentId,
+    content_name: contentName,
+    content_category: contentCategory,
+    value: total,
+    currency: currency,
+    num_items: totalQuantity,
+    contents: fbItems
+  },
+  tiktok: {
+    content_type: 'product_group',
+    content_id: contentId,
+    content_name: contentName,
+    content_category: contentCategory,
+    value: total,
+    currency: currency,
+    quantity: totalQuantity,
+    contents: tiktokItems
+  },
+  pinterest: {
+    product_ids: contentId,
+    product_names: contentName,
+    value: total,
+    product_quantity: totalQuantity,
+    product_category: contentCategory,
+    line_items: pinterestItems
+  },
+  snapchat: {
+    item_ids: contentId,
+    item_category: contentCategory,
+    price: total,
+    number_items: totalQuantity
+  },
+});
+
+});
+
+//add to cart event setup
+analytics.subscribe("product_added_to_cart", (event) => {
+  console.log('custom event', event)
+
+  var currencyToCountry = {
+  'USD': 'US', 'EUR': 'NL', 'BDT': 'BD', 'GBP': 'GB', 'CAD': 'CA', 'AUD': 'AU', 'JPY': 'JP',
+  'INR': 'IN', 'CNY': 'CN', 'SEK': 'SE', 'NOK': 'NO', 'CHF': 'CH', 'SGD': 'SG', 'BRL': 'BR',
+  'ZAR': 'ZA', 'RUB': 'RU', 'AED': 'AE', 'KRW': 'KR', 'MXN': 'MX', 'TRY': 'TR', 'MYR': 'MY',
+  'NZD': 'NZ', 'HKD': 'HK', 'DKK': 'DK', 'PLN': 'PL', 'THB': 'TH', 'EGP': 'EG', 'ILS': 'IL',
+  'IDR': 'ID', 'CZK': 'CZ', 'HUF': 'HU', 'ARS': 'AR', 'CLP': 'CL', 'PHP': 'PH', 'COP': 'CO',
+  'RSD': 'RS', 'QAR': 'QA', 'BHD': 'BH', 'KWD': 'KW', 'OMR': 'OM', 'PKR': 'PK', 'SAR': 'SA'
+};
+
+var products = event.data.cartLine;
+var itemsArray = [];
+var total = event.data.cartLine.cost.totalAmount.amount;
+var currency = event.data.cartLine.cost.totalAmount.currencyCode;
+var totalQuantity = event.data.cartLine.quantity;
+
+  var item_variant = products.merchandise.id;
+  var item_id = products.merchandise.product.id;
+  var item_name = products.merchandise.product.title;
+  var item_category = products.merchandise.product.type;
+  var item_brand = products.merchandise.product.vendor;
+  var price = products.merchandise.price.amount;
+  var quantity = products.quantity;
+
+  var productObject = {
+    item_id: item_id,
+    item_variant: item_variant,
+    item_name: item_name,
+    item_category: item_category,
+    item_brand: item_brand,
+    item_delivery: 'in_store',
+    price: price,
+    quantity: quantity
+  };
+
+  itemsArray.push(productObject);
+
+
+// Get the country code based on the currency code
+var countryCode = currencyToCountry[currency] || 'US'; // Default to US if the currency is not found
+
+// Generate the dynamic Google Ads Remarketing id
+var gadsItems = itemsArray.map(function(product) {
+  return {
+    id: 'shopify_' + countryCode + '_' + product.item_id + '_' + product.item_variant,
+    google_business_vertical: 'retail'
+  };
+});
+
+// Create arrays and string for content_ids, content_names, and content categories
+var contentId = itemsArray.map(function(product) {
+  return product.item_id;
+}).length === 1 ? itemsArray[0].item_id : itemsArray.map(product => product.item_id);
+
+var contentName = itemsArray.map(function(product) {
+  return product.item_name;
+}).length === 1 ? itemsArray[0].item_name : itemsArray.map(product => product.item_name);
+
+var uniqueCategories = [...new Set(itemsArray.map(product => product.item_category))];
+var contentCategory = uniqueCategories.length === 1 ? uniqueCategories[0] : uniqueCategories.join(', ');
+
+
+  // Facebook schema data
+var fbItems = itemsArray.map(function(product) {
+  return {
+    id: product.item_id,
+    item_price: product.price,
+    delivery_category: product.item_delivery,
+    quantity: product.quantity || 1
+  };
+});
+  
+// Tiktok contents build-up
+var tiktokItems = itemsArray.map(function(product) {
+  return {
+    content_id: product.item_id,
+    content_name: product.item_name,
+    content_category: product.item_category,
+    brand: product.item_brand,
+    price: product.price,
+    quantity: product.quantity || 1
+  };
+});
+
+// Pinterest contents array build-up
+var pinterestItems = itemsArray.map(function(product) {
+  return {
+    product_id: product.item_id,
+    product_name: product.item_name,
+    product_price: product.price,
+    product_brand: product.item_brand,
+    product_quantity: product.quantity || 1,
+    product_category: product.item_category,
+  };
+})
+
+  // view cart dataLayer
+window.dataLayer.push({
+  event: 'add_to_cart',
+  client_id: event.clientId,
+  event_id: event.id,
+  time_stamp: event.timestamp,
+  time_stamp_unix: Math.round(new Date(event.timestamp).getTime() / 1000), 
+  page_location: event.context.document.location.href,
+  page_path: event.context.document.location.pathname,
+  page_hostname: event.context.document.location.hostname,
+  fired_from: 'custom_event',
+  ecommerce: {
+    value: total,
+    currency: currency,
+    items: itemsArray
+  },
+  google_ads: {
+    remarketing: gadsItems
+  },
+  facebook: {
+    content_type: 'product_group',
+    content_ids: contentId,
+    content_name: contentName,
+    content_category: contentCategory,
+    value: total,
+    currency: currency,
+    num_items: totalQuantity,
+    contents: fbItems
+  },
+  tiktok: {
+    content_type: 'product_group',
+    content_id: contentId,
+    content_name: contentName,
+    content_category: contentCategory,
+    value: total,
+    currency: currency,
+    quantity: totalQuantity,
+    contents: tiktokItems
+  },
+  pinterest: {
+    product_ids: contentId,
+    product_names: contentName,
+    value: total,
+    product_quantity: totalQuantity,
+    product_category: contentCategory,
+    line_items: pinterestItems
+  },
+  snapchat: {
+    item_ids: contentId,
+    item_category: contentCategory,
+    price: total,
+    number_items: totalQuantity
+  },
+});
+});
+
+// view cart event setup
+analytics.subscribe('cart_viewed', (event) => {
+  console.log('custom event', event)
+ 
+var currencyToCountry = {
+  'USD': 'US', 'EUR': 'DE', 'BDT': 'BD', 'GBP': 'UK', 'CAD': 'CA', 'AUD': 'AU', 'JPY': 'JP',
+  'INR': 'IN', 'CNY': 'CN', 'SEK': 'SE', 'NOK': 'NO', 'CHF': 'CH', 'SGD': 'SG', 'BRL': 'BR',
+  'ZAR': 'ZA', 'RUB': 'RU', 'AED': 'AE', 'KRW': 'KR', 'MXN': 'MX', 'TRY': 'TR', 'MYR': 'MY',
+  'NZD': 'NZ', 'HKD': 'HK', 'DKK': 'DK', 'PLN': 'PL', 'THB': 'TH', 'EGP': 'EG', 'ILS': 'IL',
+  'IDR': 'ID', 'CZK': 'CZ', 'HUF': 'HU', 'ARS': 'AR', 'CLP': 'CL', 'PHP': 'PH', 'COP': 'CO',
+  'RSD': 'RS', 'QAR': 'QA', 'BHD': 'BH', 'KWD': 'KW', 'OMR': 'OM', 'PKR': 'PK'
+};
+
+var products = event.data.cart.lines;
+var itemsArray = [];
+var total = event.data.cart.cost.totalAmount.amount;
+var currency = event.data.cart.cost.totalAmount.currencyCode;
+var totalQuantity = event.data.cart.totalQuantity;
+
+for (var i = 0; i < products.length; i++) {
+  var item_variant = products[i].merchandise.id;
+  var item_id = products[i].merchandise.product.id;
+  var item_name = products[i].merchandise.product.title;
+  var item_category = products[i].merchandise.product.type;
+  var item_brand = products[i].merchandise.product.vendor;
+  var price = products[i].merchandise.price.amount;
+  var quantity = products[i].quantity;
+
+  var productObject = {
+    item_id: item_id,
+    item_variant: item_variant,
+    item_name: item_name,
+    item_category: item_category,
+    item_brand: item_brand,
+    item_delivery: 'in_store',
+    price: price,
+    quantity: quantity
+  };
+
+  itemsArray.push(productObject);
+}
+
+// Get the country code based on the currency code
+var countryCode = currencyToCountry[currency] || 'US'; // Default to US if the currency is not found
+
+// Generate the dynamic Google Ads Remarketing id
+var gadsItems = itemsArray.map(function(product) {
+  return {
+    id: 'shopify_' + countryCode + '_' + product.item_id + '_' + product.item_variant,
+    google_business_vertical: 'retail'
+  };
+});
+
+// Create arrays and string for content_ids, content_names, and content categories
+var contentId = itemsArray.map(function(product) {
+  return product.item_id;
+}).length === 1 ? itemsArray[0].item_id : itemsArray.map(product => product.item_id);
+
+var contentName = itemsArray.map(function(product) {
+  return product.item_name;
+}).length === 1 ? itemsArray[0].item_name : itemsArray.map(product => product.item_name);
+
+var uniqueCategories = [...new Set(itemsArray.map(product => product.item_category))];
+var contentCategory = uniqueCategories.length === 1 ? uniqueCategories[0] : uniqueCategories.join(', ');
+
+  // Facebook schema data
+var fbItems = itemsArray.map(function(product) {
+  return {
+    id: product.item_id,
+    item_price: product.price,
+    delivery_category: product.item_delivery,
+    quantity: product.quantity || 1
+  };
+});
+  
+// Tiktok contents build-up
+var tiktokItems = itemsArray.map(function(product) {
+  return {
+    content_id: product.item_id,
+    content_name: product.item_name,
+    content_category: product.item_category,
+    brand: product.item_brand,
+    price: product.price,
+    quantity: product.quantity || 1
+  };
+});
+
+// Pinterest contents array build-up
+var pinterestItems = itemsArray.map(function(product) {
+  return {
+    product_id: product.item_id,
+    product_name: product.item_name,
+    product_price: product.price,
+    product_brand: product.item_brand,
+    product_quantity: product.quantity || 1,
+    product_category: product.item_category,
+  };
+})
+
+  // view cart dataLayer
+window.dataLayer.push({
+  event: 'view_cart',
+  client_id: event.clientId,
+  event_id: event.id,
+  time_stamp: event.timestamp,
+  time_stamp_unix: Math.round(new Date(event.timestamp).getTime() / 1000), 
+  page_location: event.context.document.location.href,
+  page_path: event.context.document.location.pathname,
+  page_hostname: event.context.document.location.hostname,
+  fired_from: 'custom_event',
+  ecommerce: {
+    value: total,
+    currency: currency,
+    items: itemsArray
+  },
+  google_ads: {
+    remarketing: gadsItems
+  },
+  facebook: {
+    content_type: 'product_group',
+    content_ids: contentId,
+    content_name: contentName,
+    content_category: contentCategory,
+    value: total,
+    currency: currency,
+    num_items: totalQuantity,
+    contents: fbItems
+  },
+  tiktok: {
+    content_type: 'product_group',
+    content_id: contentId,
+    content_name: contentName,
+    content_category: contentCategory,
+    value: total,
+    currency: currency,
+    quantity: totalQuantity,
+    contents: tiktokItems
+  },
+  pinterest: {
+    product_ids: contentId,
+    product_names: contentName,
+    value: total,
+    product_quantity: totalQuantity,
+    product_category: contentCategory,
+    line_items: pinterestItems
+  },
+  snapchat: {
+    item_ids: contentId,
+    item_category: contentCategory,
+    price: total,
+    number_items: totalQuantity
+  },
+}); 
+});
+
+// remove from cart event setup
+analytics.subscribe("product_removed_from_cart", (event) => {
+  console.log('custom event', event)
+
+var currencyToCountry = {
+  'USD': 'US', 'EUR': 'DE', 'BDT': 'BD', 'GBP': 'UK', 'CAD': 'CA', 'AUD': 'AU', 'JPY': 'JP',
+  'INR': 'IN', 'CNY': 'CN', 'SEK': 'SE', 'NOK': 'NO', 'CHF': 'CH', 'SGD': 'SG', 'BRL': 'BR',
+  'ZAR': 'ZA', 'RUB': 'RU', 'AED': 'AE', 'KRW': 'KR', 'MXN': 'MX', 'TRY': 'TR', 'MYR': 'MY',
+  'NZD': 'NZ', 'HKD': 'HK', 'DKK': 'DK', 'PLN': 'PL', 'THB': 'TH', 'EGP': 'EG', 'ILS': 'IL',
+  'IDR': 'ID', 'CZK': 'CZ', 'HUF': 'HU', 'ARS': 'AR', 'CLP': 'CL', 'PHP': 'PH', 'COP': 'CO',
+  'RSD': 'RS', 'QAR': 'QA', 'BHD': 'BH', 'KWD': 'KW', 'OMR': 'OM', 'PKR': 'PK'
+};
+
+var products = event.data.cartLine; // Assuming 'cartLine' is an array
+var itemsArray = [];
+var total = event.data.cartLine.cost.totalAmount.amount;
+var currency = event.data.cartLine.cost.totalAmount.currencyCode;
+var totalQuantity = event.data.cartLine.quantity;
+
+
+  var item_variant = products.merchandise.id;
+  var item_id = products.merchandise.product.id;
+  var item_name = products.merchandise.product.title;
+  var item_category = products.merchandise.product.type;
+  var item_brand = products.merchandise.product.vendor;
+  var price = products.merchandise.price.amount;
+  var quantity = products.quantity;
+
+  var productObject = {
+    item_id: item_id,
+    item_variant: item_variant,
+    item_name: item_name,
+    item_category: item_category,
+    item_brand: item_brand,
+    item_delivery: 'in_store',
+    price: price,
+    quantity: quantity
+  };
+
+  itemsArray.push(productObject);
+
+
+
+// Get the country code based on the currency code
+var countryCode = currencyToCountry[currency] || 'US'; // Default to US if the currency is not found
+
+// Generate the dynamic Google Ads Remarketing id
+var gadsItems = itemsArray.map(function(product) {
+  return {
+    id: 'shopify_' + countryCode + '_' + product.item_id + '_' + product.item_variant,
+    google_business_vertical: 'retail'
+  };
+});
+
+// Create arrays and string for content_ids, content_names, and content categories
+var contentId = itemsArray.map(function(product) {
+  return product.item_id;
+}).length === 1 ? itemsArray[0].item_id : itemsArray.map(product => product.item_id);
+
+var contentName = itemsArray.map(function(product) {
+  return product.item_name;
+}).length === 1 ? itemsArray[0].item_name : itemsArray.map(product => product.item_name);
+
+var uniqueCategories = [...new Set(itemsArray.map(product => product.item_category))];
+var contentCategory = uniqueCategories.length === 1 ? uniqueCategories[0] : uniqueCategories.join(', ');
+
+
+
+  // Facebook schema data
+var fbItems = itemsArray.map(function(product) {
+  return {
+    id: product.item_id,
+    item_price: product.price,
+    delivery_category: product.item_delivery,
+    quantity: product.quantity || 1
+  };
+});
+  
+
+  snapchat: {
+    item_ids: contentId,
+    item_category: contentCategory,
+    price: total,
+    number_items: totalQuantity
+  }
+  });  
+});
