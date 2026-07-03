84a7d0f6b3a655ea54cb2852f60496c3399059a7
asraful <asrafulasraful166@gmail.com>
2026-07-03 10:36:29 +0600

    Rest solve

--- Object/Rest_operator.js
+++ Object/Rest_operator.js
@@ -4,3 +4,11 @@ const user = {
   city: "Dhaka",
   country: "Bangladesh"
 };
+
+const { name, ...rest } = user;
+
+console.log(name);
+// Asraful
+
+console.log(rest);
+// { age: 22, city: "Dhaka", country: "Bangladesh" }
