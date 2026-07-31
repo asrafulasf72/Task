b6549af19ca47943264e616b48a887e4816237cf
asraful <asrafulasraful166@gmail.com>
2026-07-31 10:50:34 +0600

    Details Description added

--- Object/problem_10.js
+++ Object/problem_10.js
@@ -11,3 +11,19 @@ const {
 
 console.log(city);
 console.log(address);
+
+/**
+ * Answer
+Dhaka
+ReferenceError: address is not defined
+💡 Why?
+
+Only city was extracted.
+
+address: { city }
+
+does not create an address variable.
+ * 
+ * 
+ * 
+ */
