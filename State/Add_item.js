2f3ab4fd3ec3af0f23ece9630be09a94b1387d38
asraful <asrafulasraful166@gmail.com>
2026-09-11 19:34:33 +0600

    create new product object and added

--- State/Add_item.js
+++ State/Add_item.js
@@ -2,3 +2,12 @@ const [cart, setCart] = useState([
   { id: 1, name: "Phone", qty: 1 },
   { id: 2, name: "Mouse", qty: 2 }
 ]);
+
+setCart(prevCart => [
+  ...prevCart,
+  {
+    id: 3,
+    name: "Keyboard",
+    qty: 1
+  }
+]);
