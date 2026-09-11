d8648d58cf5e57add69fd86566ea9f8fdf6abdfb
asraful <asrafulasraful166@gmail.com>
2026-09-11 19:36:58 +0600

    remove cart statr declear

--- State/Add_&_Remove_item.js
+++ State/Add_&_Remove_item.js
@@ -11,3 +11,12 @@ setCart(prevCart => [
     qty: 1
   }
 ]);
+
+
+/**Remove From Cart */
+
+const [removecart, setRemoveCart] = useState([
+  { id: 1, name: "Phone", qty: 1 },
+  { id: 2, name: "Mouse", qty: 2 },
+  { id: 3, name: "Keyboard", qty: 1 }
+]);
